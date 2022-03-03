5eca23c1be82fcf557ed2c85ddf1492879e7fa23
James Bush <bushjam@oregonstate.edu>
2022-03-02 20:01:42 -0800

    retrieve template converts values for membershipStatus and eeStatus

--- database/38_db.sql
+++ database/38_db.sql
@@ -11,7 +11,7 @@ CREATE TABLE `Customers` (
   `customerFirstName` varchar(35) NOT NULL,
   `customerLastName` varchar(35) NOT NULL,
   `customerEmail` varchar(254),
- `membershipStatus` tinyint(1) NOT NULL,
+  `membershipStatus` tinyint(1) NOT NULL,
   PRIMARY KEY (`customerID`)
 ) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=latin1;
 
